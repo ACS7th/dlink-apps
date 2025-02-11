bd8e0ae81432259704f22260ac87cf7ecd320490
dealim <dealimmmm@gmail.com>
2025-02-11 09:16:52 +0900

    chore: add skeleton for alcohols api

--- spring-app/alcohols/src/main/java/site/dlink/AlcoholsApplication.java
+++ spring-app/alcohols/src/main/java/site/dlink/AlcoholsApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"site.dlink.alcohols", "site.dlink.common"})
 public class AlcoholsApplication {
 
 	public static void main(String[] args) {
