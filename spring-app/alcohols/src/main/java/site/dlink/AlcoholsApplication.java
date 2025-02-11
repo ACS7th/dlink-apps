65f1131c903e0ffcd4c66df8e1dca97e881c9623
dealim <dealimmmm@gmail.com>
2025-02-11 10:11:38 +0900

    feat: convert to Mongo ODM

--- spring-app/alcohols/src/main/java/site/dlink/AlcoholsApplication.java
+++ spring-app/alcohols/src/main/java/site/dlink/AlcoholsApplication.java
@@ -2,6 +2,7 @@ package site.dlink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"site.dlink.alcohols", "site.dlink.common"})
--- spring-app/auth/src/main/java/site/dlink/auth/repository/UserRepository.java
+++ spring-app/auth/src/main/java/site/dlink/auth/repository/UserRepository.java
@@ -2,13 +2,15 @@ package site.dlink.auth.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import site.dlink.auth.entity.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>{
-    Optional<User> findByUserId(Long userId);
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findById(String id);
+
     Optional<User> findByEmail(String email);
 }
