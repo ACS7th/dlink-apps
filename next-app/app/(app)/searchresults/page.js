6427ac4d9084c17267105e42618bc7edfaa4f519
seunghun <leen7894671@gmail.com>
2025-02-12 16:06:01 +0900

    feat: add searchresult-page

--- next-app/app/(app)/searchresults/page.js
+++ next-app/app/(app)/searchresults/page.js
@@ -1,14 +1,28 @@
 "use client";
 
 import Content from "@/components/home/content";
-import SearchResults from "@/components/searchresults/searchResults";
+import YangjuSearchResults from "@/components/yangjus-searchresults/searchResults";
+import WineSearchResults from "@/components/wines-searchresults/searchResults";
 import { ScrollShadow } from "@heroui/react";
+import { Tabs, Tab } from "@heroui/tabs";
 
 export default function Search() {
+  const tabs = [
+    { id: "Wine", label: "Wine" },
+    { id: "Yangju", label: "Yangju" },
+  ];
 
   return (
     <ScrollShadow className="w-full h-[90vh]">
-      <SearchResults />
+      <div className="flex w-full flex-col p-1 rounded-md shadow-md">
+        <Tabs aria-label="Dynamic tabs" items={tabs} fullWidth>
+          {tabs.map((tab) => (
+            <Tab key={tab.id} title={tab.label}>
+              {tab.id === "Wine" ? <WineSearchResults /> : <YangjuSearchResults />}
+            </Tab>
+          ))}
+        </Tabs>
+      </div>
     </ScrollShadow>
-  )
+  );
 }
--- next-app/next.config.js
+++ next-app/next.config.js
@@ -8,9 +8,10 @@ const SPRING_HOST = process.env.SPRING_URI
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["lh3.googleusercontent.com", SPRING_HOST].filter(Boolean), 
+    domains: ["lh3.googleusercontent.com", SPRING_HOST].filter(Boolean),
+    domains: ['kaja2002.com'],
   },
-  
+
 };
 
 module.exports = nextConfig;
