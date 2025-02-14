12f1d95e7bf98fcb92ba254803460e97fb7a396c
dealim <dealimmmm@gmail.com>
2025-02-14 15:32:47 +0900

    feat: like count API

--- spring-app/highball/src/main/java/site/dlink/repository/HighballRepository.java
+++ spring-app/highball/src/main/java/site/dlink/repository/HighballRepository.java
@@ -3,10 +3,14 @@ package site.dlink.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import site.dlink.document.Highball;
 import site.dlink.enums.HighballCateEnum;
 
 public interface HighballRepository extends MongoRepository<Highball, String> {
     List<Highball> findByCategory(HighballCateEnum category);
+
+    @Query(value = "{ '_id': ?0 }", fields = "{ 'likedUsers': 1 }")
+    Highball findLikedUsersById(String highballId);
 }
